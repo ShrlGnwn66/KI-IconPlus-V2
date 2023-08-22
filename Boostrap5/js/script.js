a8dabfe5ac1a4b8f337dc1a54c91310b357af92c
SahrulGunawan10 <sg3533538@gmail.com>
2023-08-22 13:29:04 +0700

    galery section

--- Boostrap5/js/script.js
+++ Boostrap5/js/script.js
@@ -8,20 +8,20 @@ $(window).scroll(function () {
 });
 
 //count
-const counters = document.querySelectorAll('.count');
+const counters = document.querySelectorAll(".count");
 const options = {
   root: null, // Default: viewport
-  rootMargin: '0px',
-  threshold: 0.5 // Jika setengah atau lebih elemen masuk ke dalam viewport, observer akan dimulai
+  rootMargin: "0px",
+  threshold: 0.5, // Jika setengah atau lebih elemen masuk ke dalam viewport, observer akan dimulai
 };
-const counter = document.querySelectorAll('.count');
+const counter = document.querySelectorAll(".count");
 const speed = 55;
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.isIntersecting) {
       const target = entry.target;
-      const targetCount = Number(target.getAttribute('data-target'));
- 
+      const targetCount = Number(target.getAttribute("data-target"));
+
       let currentCount = 0;
       const updateCounter = () => {
         if (currentCount < targetCount) {
@@ -38,7 +38,62 @@ const observer = new IntersectionObserver(entries => {
     }
   });
 }, options);
-counters.forEach(counter => {
+counters.forEach((counter) => {
   observer.observe(counter);
 });
 //end count
+
+// Gallery Items Filter
+
+// const filterButtons = document.querySelector("#galery-btns").children;
+// const items = document.querySelector(".galeri-image").children;
+
+// for (let i = 0; i < filterButtons.length; i++) {
+//   filterButtons[i].addEventListener("click", function () {
+//     for (let j = 0; j < filterButtons.length; j++) {
+//       filterButtons[j].classList.remove("active");
+//     }
+//     this.classList.add("active");
+//     const target = this.getAttribute("data-target");
+
+//     for (let k = 0; k < items.length; k++) {
+//       items[k].style.display = "none";
+//       if (target == items[k].getAttribute("data-id")) {
+//         items[k].style.display = "block";
+//       }
+
+//       if (target === "All") {
+//         items[k].style.display = "block";
+//       }
+//     }
+//   });
+// }
+
+// Gallery Items Filter
+const filterButtons = document.querySelector("#galery-btns").children;
+const items = document.querySelector(".galeri-image").children;
+
+for (let k = 0; k < items.length; k++) {
+  if (items[k].getAttribute("data-id") === "DKV") {
+    items[k].style.display = "block";
+  } else {
+    items[k].style.display = "none";
+  }
+}
+
+for (let i = 0; i < filterButtons.length; i++) {
+  filterButtons[i].addEventListener("click", function () {
+    for (let j = 0; j < filterButtons.length; j++) {
+      filterButtons[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const target = this.getAttribute("data-target");
+
+    for (let k = 0; k < items.length; k++) {
+      items[k].style.display = "none";
+      if (target === items[k].getAttribute("data-id") || target === "All") {
+        items[k].style.display = "block";
+      }
+    }
+  });
+}
