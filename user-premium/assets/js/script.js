0759ccc1f82f1a8709b87e61e7afcbc97597848e
lunamufii <zidnimshtf@gmail.com>
2023-09-05 14:52:09 +0700

    deleting file backup

--- user-premium/assets/js/script.js
+++ user-premium/assets/js/script.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const bottomNavbar = document.querySelector(".custom_nav-menu");
+  const topNavbarHeight = document.querySelector(".custom_nav-one").clientHeight;
+
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > topNavbarHeight) {
+          bottomNavbar.classList.add("fixed");
+      } else {
+          bottomNavbar.classList.remove("fixed");
+      }
+  });
+});
+
 // Pesan eror nama instansi
 const namaInstansiInput = document.getElementById("nama-instansi");
 const namaInstansiError = document.getElementById("nama-instansi-error");
