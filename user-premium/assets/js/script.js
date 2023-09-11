3cd67f15cfef916014ee9e1ea0ccd341a5f8c657
SahrulGunawan10 <sg3533538@gmail.com>
2023-09-11 08:50:41 +0700

    fix link in form and add js validation

--- user-premium/assets/js/script.js
+++ user-premium/assets/js/script.js
@@ -136,31 +136,3 @@ function tampilData() {
     alert("Semua data harus di isi terlebih dahulu");
   }
 }
-
-// Pesan eror nama instansi
-// const namaInstansiInput = document.getElementById("nama-instansi");
-// const namaInstansiError = document.getElementById("nama-instansi-error");
-
-// namaInstansiInput.addEventListener("input", function () {
-//   if (namaInstansiInput.validity.valueMissing) {
-//     namaInstansiError.textContent = "Nama Instansi tidak boleh kosong";
-//   } else {
-//     namaInstansiError.textContent = console.log("Inputan Sesuai");
-//   }
-// });
-
-// // Pesan eror Periode Magang
-// const tanggalMulaiInput = document.getElementById("tanggal-mulai");
-// const tanggalSelesaiInput = document.getElementById("tanggal-selesai");
-// const tanggalError = document.getElementById("tanggal-error");
-
-// tanggalSelesaiInput.addEventListener("input", function () {
-//   const tanggalMulai = new Date(tanggalMulaiInput.value);
-//   const tanggalSelesai = new Date(this.value);
-
-//   if (tanggalSelesai < tanggalMulai) {
-//     tanggalError.textContent = "Tanggal Selesai harus setelah Tanggal Mulai";
-//   } else {
-//     tanggalError.textContent = "";
-//   }
-// });
