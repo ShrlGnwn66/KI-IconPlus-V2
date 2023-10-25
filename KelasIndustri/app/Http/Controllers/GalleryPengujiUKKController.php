7dfd505a33baa600579c8151acb70379d06398ab
SahrulGunawan10 <sg3533538@gmail.com>
2023-10-25 15:34:55 +0700

    solv error

--- KelasIndustri/app/Http/Controllers/GalleryPengujiUKKController.php
+++ KelasIndustri/app/Http/Controllers/GalleryPengujiUKKController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\galleryPengujiUKK;
+use App\Models\GalleryPengujiUKK;
 use Illuminate\Http\Request;
 
 class GalleryPengujiUKKController extends Controller
